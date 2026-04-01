a7b43f771065583f0d9d7ddc0afe050b6b456259
arvind <erwin09chauhan@gmail.com>
2026-04-02 12:53:06 +1300

    Initial solution structure with Clean Architecture project skeletons

---
+++ tests/JobApplicationTracker.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace JobApplicationTracker.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/JobApplicationTracker.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace JobApplicationTracker.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
